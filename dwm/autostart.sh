c1cac454c2c775890a6fc7c2a1dafc763e7916cb
4Dk3 <82821885+4Dk3@users.noreply.github.com>
2022-01-14 11:05:30 -0500

    Add files via upload

---
+++ dwm/autostart.sh
@@ -0,0 +1,10 @@
+dwmblocks &
+feh --bg-fill ~/Wallpapers/0005.jpg &
+picom -b -f --experimental-backends &
+lxpolkit &
+nm-applet &
+albert &
+dunst &
+xfce4-power-manager &
+flameshot &
+sh ~/Scripts/Create_Res_75Hz.sh
