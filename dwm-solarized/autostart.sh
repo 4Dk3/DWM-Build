b88e831f82e52f18fa28808a61693c9804f45238
4Dk3 <82821885+4Dk3@users.noreply.github.com>
2022-01-19 03:55:05 +0000

    Add files via upload

---
+++ dwm-solarized/autostart.sh
@@ -0,0 +1,10 @@
+dwmblocks &
+feh --bg-fill ~/Wallpapers/solarized-sword.png &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+lxpolkit &
+albert &
+dunst &
+xfce4-power-manager &
+flameshot &
+sh ~/Scripts/Create_Res_75Hz.sh
+picom -b -f --experimental-backends &
